d1ac56dbdd8694b5edb2509d635c4821b717a2cc
Svitlana <153005758+svitlanaponom@users.noreply.github.com>
2024-04-02 22:48:17 +0300

    Delete cypress/e2e/wiki.cy.js

--- cypress/e2e/wiki.cy.js
+++
@@ -1,9 +0,0 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://www.wikipedia.org/')
-    cy.get('#js-link-box-en').click();
-    cy.get('#pt-createaccount-2').should('be.visible');
-    cy.get('#pt-login-2').should('be.visible');
-    cy.get('#vector-user-links-dropdown').should('be.visible');
-  })
-})
